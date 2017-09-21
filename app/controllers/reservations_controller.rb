95267a8c7c413e2a8f955a23f9d9ba3ffd269af4
GeorgeSalari <x3m.giku@gmail.com>
2017-09-22 00:12:27 +0800

    move all check from controller to model

--- app/controllers/reservations_controller.rb
+++ app/controllers/reservations_controller.rb
@@ -1,12 +1,27 @@
 class ReservationsController < ApplicationController
+  def index
+    @reservations = Reservation.where(user_id: params[:user_id])
+  end
+
   def new
     @listing = Listing.find(params[:listing_id])
     @reservation = Reservation.new
   end
 
   def create
-    byebug
     @reservation = Reservation.new(reservation_params)
+    params[:reservation][:end_date] = Date.strptime(reservation_params[:end_date], '%m/%d/%Y')
+    params[:reservation][:start_date] = Date.strptime(reservation_params[:start_date], '%m/%d/%Y')
+    @reservation = Reservation.new(reservation_params)
+    if @reservation.save
+      redirect_to user_reservation_path(current_user, @reservation)
+    else
+      render template: "reservations/new"
+    end
+  end
+
+  def show
+    @reservation = Reservation.find(params[:id])
   end
 
   private
--- app/models/listing.rb
+++ app/models/listing.rb
@@ -3,4 +3,26 @@ class Listing < ApplicationRecord
   has_many :reservations
   self.per_page = 10
   mount_uploaders :images, ListingUploader
+
+  def self.check_user_status(city, user)
+    if user.customer?
+      if city.nil? || city.empty?
+        listings = Listing.where(verification: true)
+      else
+        listings = Listing.where(city: city, verification: true)
+      end
+    elsif user.moderator?
+      if city.nil? || city.empty?
+        listings = Listing.where(verification: false)
+      else
+        listings = Listing.where(city: city, verification: false)
+      end
+    else
+      if city.nil? || city.empty?
+        listings = Listing.all
+      else
+        listings = Listing.where(city: city)
+      end
+    end
+  end
 end
