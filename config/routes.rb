79b5a64a4a0f502d644941e6003109591f8d8b40
GeorgeSalari <x3m.giku@gmail.com>
2017-09-26 14:08:29 +0800

    generate controller Braintree with method new and in view folder braintree with new html erb

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'braintree/new'
+
   resources :passwords, controller: "clearance/passwords", only: [:create, :new]
   resource :session, controller: "sessions", only: [:create]
 
